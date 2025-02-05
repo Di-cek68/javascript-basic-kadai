4f01cfc594490394b5ad4a943095a378a995e2cf
Di-cek68 <dytydyty6791@gmail.com>
2025-02-05 21:56:21 +0900

    kadai_020/event.html
    kadai_020/event.html

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,9 @@
-btn.addEventListener('click', () => {
-    console.log('ボタンをクリックしました');
-    // メッセージ表示用の要素にメッセージを設定
-    text.textContent = 'ボタンをクリックしました';
+// 1. HTML要素を取得
+const btn = document.getElementById("btn");
+const text = document.getElementById("text");
+
+// 2. イベントリスナーを設定
+btn.addEventListener("click", function() {
+  // 3. イベント発生時の処理を記述
+  text.textContent = "ボタンをクリックしました";
 });
